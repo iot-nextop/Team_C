fd3d0d99ce9c0bbe35b24a1ff2fed43c38dcb38e
이순형 <tnsgud9@naver.com>
2019-09-22 14:24:23 +0900

    Project Start

---
+++ WeatherBox/WeatherBox.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		WeatherBox.ino
+ Created:	2019-09-22 오후 2:23:09
+ Author:	user
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
